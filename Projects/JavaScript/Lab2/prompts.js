c0a29ec6b9563afc08460e2520dc4c356111d8b0
KerryFerguson <924056@students.nccvt.k12.de.us>
2020-12-14 13:45:42 -0500

    Completed Prompt 2.

--- Projects/JavaScript/Lab2/prompts.js
+++ Projects/JavaScript/Lab2/prompts.js
@@ -1,3 +1,7 @@
 //Prompt a user twice to input a number. Store their response in variables.
 let num1 = prompt("Enter a number");
 let num2 = prompt("Enter another number");
+
+//Declare another variable to store the total of each statement
+let sum = parseInt(num1) + parseInt(num2);
+alert(sum);
