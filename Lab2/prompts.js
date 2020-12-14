68dbc42d9727414f9c9ee9dee048d7b41264cbba
KerryFerguson <924056@students.nccvt.k12.de.us>
2020-12-14 13:54:45 -0500

    Completed Prompt 2.

--- Lab2/prompts.js
+++ Lab2/prompts.js
@@ -2,3 +2,6 @@
 let num1 = prompt("Enter a number");
 let num2 = prompt("Enter another number");
 
+//Declare another variable to store the total of each statement
+let sum = parseInt(num1) + parseInt(num2);
+alert(sum);
